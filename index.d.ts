a2d2af0b55d29bb4d296778bece5a76a9902877a
Eloy Durán <eloy.de.enige@gmail.com>
2020-02-15 01:04:15 +0100

    Add some RN specific ones for now

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,7 @@
 export type $TEMPORARY$object<T extends object> = T;
 export type $TEMPORARY$string<T extends string> = T;
 export type $TEMPORARY$array<T extends any[]> = T;
+
+// TODO: These are actually just RN types
+export type $FlowFixMeProps = any;
+export type $FlowFixMeState = any;
