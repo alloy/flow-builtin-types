62713e4cbeba526cf5da3226ea701440c5c4b0b5
Eloy Durán <eloy.de.enige@gmail.com>
2020-02-15 11:32:02 +0100

    Add another RN internal type

--- index.d.ts
+++ index.d.ts
@@ -7,6 +7,7 @@ export type $TEMPORARY$array<T extends any[]> = T;
 // TODO: These are actually just RN types
 export type $FlowFixMeProps = any;
 export type $FlowFixMeState = any;
+export type Stringish = string;
 
 // TGODO: These are actually prop-types types
 export type React$PropType$Primitive<T> = PropTypes.Requireable<T>
