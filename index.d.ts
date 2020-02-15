29c2b5324811d6ea5de65ea6af5507de26a37356
Eloy Durán <eloy.de.enige@gmail.com>
2020-02-15 11:41:05 +0100

    Add Flow ignore types

--- index.d.ts
+++ index.d.ts
@@ -5,8 +5,11 @@ export type $TEMPORARY$string<T extends string> = T;
 export type $TEMPORARY$array<T extends any[]> = T;
 
 // TODO: These are actually just RN types
+export type $FlowIssue = any;
+export type $FlowFixMe = any;
 export type $FlowFixMeProps = any;
 export type $FlowFixMeState = any;
+export type $FlowFixMeEmpty = any;
 export type Stringish = string;
 
 // TGODO: These are actually prop-types types
