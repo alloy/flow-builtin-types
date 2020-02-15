7313f9dfcbdb1472b0c4fce7443bfebb66304f81
Eloy Durán <eloy.de.enige@gmail.com>
2020-02-15 11:16:15 +0100

    Add used type from prop-types

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types"
+
 export type $TEMPORARY$object<T extends object> = T;
 export type $TEMPORARY$string<T extends string> = T;
 export type $TEMPORARY$array<T extends any[]> = T;
@@ -5,3 +7,6 @@ export type $TEMPORARY$array<T extends any[]> = T;
 // TODO: These are actually just RN types
 export type $FlowFixMeProps = any;
 export type $FlowFixMeState = any;
+
+// TGODO: These are actually prop-types types
+export type React$PropType$Primitive<T> = PropTypes.Requireable<T>
